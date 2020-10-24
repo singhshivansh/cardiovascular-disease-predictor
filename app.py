72c0f7395ee5497958487e9478a800b0a6fd642d
singhsivansh12 <singhshivansh12@gmail.com>
2020-10-24 23:18:04 +0530

    first commit

--- app.py
+++ app.py
@@ -1,56 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 23 01:25:05 2020
-
-@author: Shivansh
-"""
+from flask import Flask, render_template, redirect, request
 import pickle
-import streamlit as st
+# from sklearn.externals import joblib
+
+
+app = Flask(__name__)
 
 pickle_in = open("cardio_vascular_model.pickle", "rb")
-classifier = pickle.load(pickle_in)
+model = pickle.load(pickle_in)
 
-def predict_cardio(age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active,
-                     bmi):
-    prediction = str(classifier.predict([[age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active, bmi]]))
-    print(prediction)
-    return prediction
+@app.route('/')
+def hello():
+	return render_template("index.html")
 
-def main():
-    st.title("CardioVascular Disease Predictor")
-    
-    html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit CardioVascular Disease Predictor ML App </h2>
-    </div>
-    """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    
-    age = st.text_input("Age", "Type Here")
-    gender = st.text_input("Gender", "Type Here")
-    height = st.text_input("Height", "Type Here")
-    weight = st.text_input("Weigth", "Type Here")
-    ap_hi = st.text_input("Diastolic Blood Pressure", "Type Here")
-    ap_lo = st.text_input("Systolic Blood Pressure", "Type Here")
-    cholesterol = st.text_input("Cholestrol", "Type Here")
-    gluc = st.text_input("Glucose", "Type Here")
-    smoke = st.text_input("Smoking", "Type Here")
-    alco = st.text_input("Alcohol", "Type Here")
-    active = st.text_input("Active", "Type Here")
-    bmi = st.text_input("BMI(Body Mass Index)", "Type Here")
-    result = ""
-    
-    if st.button("Predict"):
-        result = predict_cardio(age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active,bmi)
-    
-    #if result == 1:
-     #   st.success("You have CVD")
-   # if result == 0:
-    #    st.success("You are Disease-free :)")
-    st.success("The Output is {}".format(result))
-    
-    
+@app.route('/', methods = ['POST'])
+def marks():
+	if request.method == 'POST' :
+		age = int(request.form['age'])
+		gender = int(request.form['gender'])
+		height = int(request.form['height'])
+		weight = float(request.form['weight'])
+		ap_hi = int(request.form['ap_hi'])
+		ap_lo = int(request.form['ap_lo'])
+		cholesterol = int(request.form['cholesterol'])
+		gluc = int(request.form['gluc'])
+		smoke = int(request.form['smoke'])
+		alco = int(request.form['alco'])
+		active = int(request.form['active'])
+		bmi = float(request.form['bmi'])
+
+		result = model.predict([[age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active, bmi]])
+
+		if result[0] == 1:
+			return render_template("index.html", result = "Ahhh..You have Cardio-Vascular Disease" )
+		else:
+			return render_template("index.html", result = "You are Disease-free!! :) :)")
+	
+
 if __name__ == '__main__':
-    main()
-    #result = predict_cardio(22469, 1, 155, 69.0, 130, 80, 2, 2, 0, 0, 1, 28.72)
-    #print(result)
+	app.run(debug = True)
